6e9d055396c80f721f4336f0f0e1947bb5a4c798
Armando Rosales <refill.1509@gmail.com>
2025-03-10 10:48:46 -0600

    [add] upload_video

--- courses/urls.py
+++ courses/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.course_list, name='course_list'),  # Página principal de cursos
-    # path('<int:course_id>/', views.course_detail, name='course_detail'),  # Detalle del curso
+        path( '' ,views.course_list ,name='course_list' ) ,  # Página principal de cursos
+        # path('<int:course_id>/', views.course_detail, name='course_detail'),  # Detalle del curso
+        path('upload/', views.upload_video, name='upload_video'),  # Subir video
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-
-from django.shortcuts import render
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 
 def course_list(request):
     return render(request, 'courses/course_list.html')
 
 # def course_detail(request, course_id):
 #     return render(request, 'courses/course_detail.html', {'course_id': course_id})
+
+
+def upload_video(request):
+    if request.method == 'POST' and request.FILES['video']:
+        video = request.FILES['video']
+        file_name = default_storage.save(video.name, ContentFile(video.read()))
+        file_url = default_storage.url(file_name)
+        print("Archivo guardado en:", file_url)
+        return render(request, 'courses/course_list.html', {'file_url': file_url})
+    return render(request, 'courses/upload_video.html')
 
