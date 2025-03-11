839af8a49999265e356ffd0be01d3383f4a7c93f
Armando Rosales <refill.1509@gmail.com>
2025-03-11 12:32:17 -0600

    [add] storage_backends.py add s3 config

---
+++ software_courses/storage_backends.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+class StaticStorage(S3Boto3Storage):
+    location = settings.STATICFILES_LOCATION
+    default_acl = "public-read"  # Para archivos estáticos públicos
+    file_overwrite = True
+
+class MediaStorage(S3Boto3Storage):
+    location = settings.MEDIAFILES_LOCATION
+    default_acl = "private"  # Los archivos multimedia suelen ser privados
+    file_overwrite = False
