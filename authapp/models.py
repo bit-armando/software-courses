5a83cc26a85f7c9f55ec3ed907f97d4fc442ca84
Armando Rosales <refill.1509@gmail.com>
2025-03-12 08:53:32 -0600

    [add] views and models in authapp

--- authapp/models.py
+++ authapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from software_courses.storage_backends import MediaStorage
+
+
+class Person(models.Model):
+    user = models.OneToOneField( User ,on_delete=models.CASCADE )
+    degrade = models.CharField(verbose_name='Degrade', max_length=255 ,blank=True ,null=True)
+    instructor = models.BooleanField( verbose_name='Is Instructor', default=False )
+    image = models.ImageField( upload_to="person_img/" ,storage=MediaStorage() ,blank=True ,null=True )
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
-# Create your views here.
+from authapp.models import Person
+
+
+def register_user(request):
+    return render(request, 'authapp/register.html')
+
+
+def login_user(request):
+    return render(request, 'authapp/login.html')
+
+
+def logout_user(request):
+    return render(request, 'authapp/logout.html')
+
+
+def new_user(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # Crear un usuario
+        user = User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password
+        )
+
+        # Crear un perfil
+        Person.objects.create(
+            user=user
+        )
+    return render(request, 'authapp/login.html')
