d4de5a612d190629c307806cb036d6d973df48ac
Armando Rosales <refill.1509@gmail.com>
2025-03-12 08:53:20 -0600

    [add] views and models in courses

--- courses/models.py
+++ courses/models.py
@@ -2,16 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from software_courses.storage_backends import MediaStorage
-
-
-class Person(models.Model):
-    user = models.OneToOneField( User ,on_delete=models.CASCADE )
-    degrade = models.CharField(verbose_name='Degrade', max_length=255 ,blank=True ,null=True)
-    instructor = models.BooleanField( verbose_name='Is Instructor', default=False )
-    image = models.ImageField( upload_to="person_img/" ,storage=MediaStorage() ,blank=True ,null=True )
-
-    def __str__(self):
-        return f'{self.user.first_name} {self.user.last_name}'
+from authapp.models import Person
 
 
 class CategoryCourse(models.Model):
--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from software_courses.storage_backends import MediaStorage
 
-from .models import Course
+from .models import Course ,Comment ,Response
+from authapp.models import Person
+
 
 def course_list(request):
     return render(request, 'courses/course_list.html')
@@ -38,3 +40,49 @@ def upload_video( request ):
         )
 
     return render( request ,'courses/upload_video.html' )
+
+
+def course_comment( request ):
+    if request.method == 'POST':
+        course_id = request.POST.get('course_id')
+        person_id = request.POST.get('person_id')
+        comment = request.POST.get('comment')
+
+        course = Course.objects.get( id=course_id )
+        person = Person.objects.get( id=person_id )
+
+        Comment.objects.create(
+            course=course,
+            person=person,
+            comment=comment
+        )
+
+    return render( request ,'courses/course.html' )
+
+
+def response_comment( request ):
+    if request.method == 'POST':
+        comment_id = request.POST.get('comment_id')
+        person_id = request.POST.get('person_id')
+        response = request.POST.get('response')
+
+        comment = Comment.objects.get( id=comment_id )
+        person = Person.objects.get( id=person_id )
+
+        Response.objects.create(
+            comment=comment,
+            person=person,
+            response=response
+        )
+
+    return render( request ,'courses/course.html' )
+
+
+def list_courses( request ):
+    courses = Course.objects.all()
+    return render( request ,'courses/list_courses.html' ,{'courses': courses} )
+
+
+def course_detail( request ,course_id ):
+    course = Course.objects.get( id=course_id )
+    return render( request ,'courses/course_detail.html' ,{'course': course} )
