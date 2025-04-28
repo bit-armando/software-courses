16abe7496047d0d21ed94796b81cc30cbb6d1737
BrandonVC214858 <al214858@alumnos.uacj.mx>
2025-04-28 14:47:44 -0600

    [add] enhance user registration and login forms with improved styling and placeholders

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,14 +1,51 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import AuthenticationForm
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput, label="Password")
-    confirm_password = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+    password = forms.CharField(
+            label="Contraseña",
+            widget=forms.PasswordInput(attrs={
+                'class': 'w-full p-3 border rounded-lg bg-white border-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500',
+                'placeholder': 'Contraseña'
+            })
+        )
+    confirm_password = forms.CharField(
+        label="Confirmar Contraseña",
+        widget=forms.PasswordInput(attrs={
+            'class': 'w-full p-3 border rounded-lg bg-white border-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500',
+            'placeholder': 'Confirmar Contraseña'
+            })
+        )
 
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password']
+        labels = {
+            'username': 'Username',
+            'email': 'Email',
+            'first_name': 'Primer Nombre',
+            'last_name': 'Apellido',
+            'password': 'Contraseña',
+        }
+        widgets = {
+            'username': forms.TextInput(attrs={
+            'class': 'w-full p-3 border rounded-lg bg-white border-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500',
+            'placeholder': 'Username'
+            }),
+            'email': forms.EmailInput(attrs={
+            'class': 'w-full p-3 border rounded-lg bg-white border-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500',
+            'placeholder': 'Email'
+            }),
+            'first_name': forms.TextInput(attrs={
+            'class': 'w-full p-3 border rounded-lg bg-white border-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500',
+            'placeholder': 'Primer Nombre'
+            }),
+            'last_name': forms.TextInput(attrs={
+            'class': 'w-full p-3 border rounded-lg bg-white border-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500',
+            'placeholder': 'Apellido'
+            }),
+        }
+
 
     def clean(self):
         cleaned_data = super().clean()
@@ -20,9 +57,21 @@ class UserRegistrationForm(forms.ModelForm):
         return cleaned_data
 
 
-class UserLoginForm(forms.Form):  # Cambiar de AuthenticationForm a forms.Form
-    email = forms.EmailField(label="Email", widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+class UserLoginForm(forms.Form):
+    email = forms.EmailField(
+        label="Email",
+        widget=forms.EmailInput(attrs={
+            'class': 'w-full p-3 border rounded-lg bg-white border-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500',
+            'placeholder': 'Email'
+        })
+    )
+    password = forms.CharField(
+        label="Contraseña",
+        widget=forms.PasswordInput(attrs={
+            'class': 'w-full p-3 border rounded-lg bg-white border-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500',
+            'placeholder': 'Contraseña'
+        })
+    )
 
     def clean(self):
         cleaned_data = super().clean()
