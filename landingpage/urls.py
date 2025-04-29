0e56ee6cf489d5bfad6de86beac79c6bde6da2f2
BrandonVC214858 <al214858@alumnos.uacj.mx>
2025-04-29 09:05:32 -0600

    feat[LandingPage] Add terms and privacy views and corresponding URL paths

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -5,4 +5,6 @@ app_name = 'landingpage'
 
 urlpatterns = [
     path('', views.home, name='home'),  # Ruta principal de la landingpage
+    path('terms/', views.terms, name='terms'),  # Ruta para los términos y condiciones
+    path('privacy/', views.privacy, name='privacy'),  # Ruta para la política de privacidad
 ]
--- landingpage/views.py
+++ landingpage/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'landingpage/home.html')
 
+def terms(request):
+    return render(request, 'landingpage/terms.html')
+
+def privacy(request):
+    return render(request, 'landingpage/privacy.html')
